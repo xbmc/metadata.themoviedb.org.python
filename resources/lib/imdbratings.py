20ce841ce191c2c8ee10e1c6206e911bf0912c88
Ryan Rector <rmrector@gmail.com>
2019-06-15 18:15:57 -0600

    fix: IMDB regex for ratings, votes, and top250

--- resources/lib/imdbratings.py
+++ resources/lib/imdbratings.py
@@ -1,9 +1,11 @@
 import requests
 import re
 
-IMDB_RATINGS_URL = 'http://www.imdb.com/title/{}/ratings'
-IMDB_RATING_VOTES_REGEX = re.compile(r'<p>(\d+).*?title\?user_rating\=.*?\">(.*?)</a>')
-IMDB_TOP250_REGEX = re.compile(r'\#(\d+).*?"/chart/top')
+IMDB_RATINGS_URL = 'https://www.imdb.com/title/{}/'
+
+IMDB_RATING_REGEX = re.compile(r'itemprop="ratingValue".*?>.*?([\d.]+).*?<')
+IMDB_VOTES_REGEX = re.compile(r'itemprop="ratingCount".*?>.*?([\d,]+).*?<')
+IMDB_TOP250_REGEX = re.compile(r'Top Rated Movies #(\d+)')
 
 # get the movie info via imdb
 def get_imdb_ratinginfo(imdb_id):
@@ -11,21 +13,26 @@ def get_imdb_ratinginfo(imdb_id):
     return _parse_imdb_result(response.text if response.status_code == 200 else '')
 
 def _parse_imdb_result(input_html):
-    rating, votes = _get_imdb_rating_and_votes(input_html)
+    rating = _get_imdb_rating(input_html)
+    votes = _get_imdb_votes(input_html)
     top250 = _get_imdb_top250(input_html)
 
     return {'votes': votes, 'rating': rating, 'top250': top250}
 
-def _get_imdb_rating_and_votes(input_html):
-    match = re.search(IMDB_RATING_VOTES_REGEX, input_html)
+def _get_imdb_rating(input_html):
+    match = re.search(IMDB_RATING_REGEX, input_html)
     if (match):
-        votes = int(match.group(1).replace(',', ''))
-        rating = float(match.group(2))
-        return rating, votes
-    return (0, 0)
+        return float(match.group(1))
+    return 0
+
+def _get_imdb_votes(input_html):
+    match = re.search(IMDB_VOTES_REGEX, input_html)
+    if (match):
+        return int(match.group(1).replace(',', ''))
+    return 0
 
 def _get_imdb_top250(input_html):
     match = re.search(IMDB_TOP250_REGEX, input_html)
     if (match):
-        return match.group(1)
+        return int(match.group(1))
     return 0

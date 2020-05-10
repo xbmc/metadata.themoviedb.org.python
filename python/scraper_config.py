65e08b4e702514a35c57e4570fc0cefc7a5d09f6
Ryan Rector <rmrector@gmail.com>
2020-05-10 16:46:45 -0600

    add artwork from fanart.tv

--- python/scraper_config.py
+++ python/scraper_config.py
@@ -7,6 +7,24 @@ def configure_scraped_details(details, settings):
     details = _configure_tags(details, settings)
     return details
 
+_fanarttv_arttypes = ['fanart', 'poster', 'clearlogo', 'clearart', 'discart', 'banner', 'landscape', 'keyart']
+_fanarttv_arttypes += ['set.' + t for t in _fanarttv_arttypes]
+def configure_fanarttv_artwork(details, settings):
+    if 'available_art' not in details:
+        return details
+    art = details['available_art']
+    for arttype in _fanarttv_arttypes:
+        if arttype in art and not settings.getSettingBool('enable_fanarttv_' + arttype):
+            del art[arttype]
+
+    return details
+
+def is_fanarttv_configured(settings):
+    for arttype in _fanarttv_arttypes:
+        if settings.getSettingBool('enable_fanarttv_' + arttype):
+            return True
+    return False
+
 def _configure_rating_prefix(details, settings):
     if details['info'].get('mpaa'):
         details['info']['mpaa'] = settings.getSettingString('certprefix') + details['info']['mpaa']
